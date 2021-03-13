1959b985c16c4062b9f7a7c1b75b17999c22c195
Phil Mansfield <phil1@stanford.edu>
2021-03-13 11:22:30 -0600

    Changed the structure of the main guppy.go file.

--- guppy.go
+++ guppy.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/phil-mansfield/guppy/lib"
 	read "github.com/phil-mansfield/guppy/go"
 )
 
 func main() {
 	// Parse arguements.
-	configFile, mode, cmdArgs := lib.ParseCommandLine()
-	configArgs := lib.ParseConfigFile(configFile)
-	rawArgs := lib.CombineArguments(configArgs, cmdArgs)
-	args := lib.ProcessArguments(rawArgs)
+	mode, configFile, cmdArgs := lib.ParseCommandLine()
+	rawArgs := lib.ParseConfigFile(configFile)
+	rawArgs.Overwrite(cmdArgs)
 
+	// Do processing that doesn't external validation.
+	args := rawArgs.Process()
+	// Tell future functions that this isn't being run with MPI.
+	args.RunMode = lib.GuppyMode
+	
 	// Run the chosen mode.
 	switch mode {
 	case "help":
-		lib.PrintHelp(lib.GuppyMode)
+		lib.PrintHelp(args)
 	case "check":
 		Check(args)
 	case "convert":
@@ -33,46 +39,51 @@ func main() {
 // Check runs guppy's "check" mode which tests for errors in the configuration
 // arguments.
 func Check(args *lib.Args) {
-	lib.Check(lib.GuppyMode, lib.CrashOnError, args)
-	fmt.Println("No errors detected.")
+	ok := lib.Check(lib.GuppyMode, args)
+	if ok {
+		fmt.Println("No errors detected.")
+	}
 }
 
 // Convert runs guppy's "convert" mode, which converts files into/out of the
 // .gup format.
 func Convert(args *lib.Args) {
-	lib.Check(lib.GuppyMode, lib.WarnOnError, args)
+	lib.Check(lib.GuppyMode, args)
 	
-	lib.StartThreads(args)
+	lib.SetThreads(args.Threads)
 
-	for _, snap := range args.Snaps {
+	for snap := 0; snap < args.Snaps; snap++ {
 		// Split particles into guppy data cubes.
-		part := lib.CollectParticles(args, snap)
-		parts := part.Split(args, part)
-		
-		for file := range parts {
+		origHd, part := lib.CollectParticles(args, snap)
+		buf := lib.SplitBuffer(args, part)
+			
+		for i, file := range args.GupFileNames {
+			// Split off
+			part.Split(args, i, buf)
+			
 			// Compress each file and write it to disk.
-			gupData := lib.Compress(args, parts[i])
-			lib.Write(args, snap, file, gupData)
+			hd := lib.CreateHeader(args, origHd, i)
+			gupData := lib.Compress(args, buf)
+			lib.Write(args, file, hd, origHd, gupData)
 		}
 	}
 }
 
 // Confirm run's guppy's "confirm" mode, which checks that
 func Confirm(args *lib.Args) {
-	lib.Check(lib.GuppyMode, lib.WarnOnError, args)
+	lib.Check(lib.GuppyMode, args)
 
-	lib.StartThreads(args)
+	lib.SetThreads(args.Threads)
 
-	for _, snap := range args.Snaps {
+	for snap := 0; snap < args.Snaps; snap++ {
 		// This is the one weak link in the "confirm" chain. If CollectParticles
 		// has a bug in it, it might not be caught through "confirm".
-		part := lib.CollectParticles(args, snap)
+		origHd, part := lib.CollectParticles(args, snap)
 
 		// Loop over all the .gup files.
-		gupFiles := lib.GupFileNames(args)
-		for file := range gupFiles {
-			gupData := read.ReadAll(fupFiles[file])
-			lib.ConfirmGupData(gupData, part)
+		for _, file := range args.GupFileNames {
+			f := read.Open(file)
+			lib.Confirm(origHd, part, f)
 		}
 	}
 	fmt.Println("No errors detected.")
