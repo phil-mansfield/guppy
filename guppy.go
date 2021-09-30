473add1629f40099797e743e4b7624b99399c23f
Phil Mansfield <phil1@stanford.edu>
2021-09-30 09:41:35 -0700

    updates to reader processes

--- guppy.go
+++ guppy.go
@@ -1,92 +1,258 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
+	"os"
+	"strings"
+	"encoding/binary"
+	
+	read_guppy "github.com/phil-mansfield/guppy/go"
 	"github.com/phil-mansfield/guppy/lib"
-	"github.com/phil-mansfield/guppy/lib/thread"
-	"github.com/phil-mansfield/guppy/lib/error"
-	read "github.com/phil-mansfield/guppy/go"
 )
 
 func main() {
-	// Parse arguements.
-	mode, configFile, cmdArgs := lib.ParseCommandLine()
-	rawArgs := lib.ParseConfigFile(configFile)
-	rawArgs.Overwrite(cmdArgs)
+	if len(os.Args) <= 1 { ModeError() }
 
-	// Do processing that doesn't external validation.
-	args := rawArgs.Process()
-	// Tell future functions that this isn't being run with MPI.
-	args.RunMode = lib.GuppyMode
+	mode := os.Args[1]
+	flags := os.Args[2:]
 	
-	// Run the chosen mode.
 	switch mode {
-	case "help":
-		lib.PrintHelp(args)
-	case "check":
-		Check(args)
-	case "convert":
-		Convert(args)
-	case "confirm":
-		Confirm(args)
+	case "read": Read(flags)
+	case "write": Write(flags)
+	case "write_server": WriteServer(flags)
 	default:
-		error.External(
-			"You attempted to run guppy in the mode '%s', but the only valid " +
-				"modes are 'help', 'check', 'convert', and 'confirm'.", mode,
-		)
+		ModeError()
 	}
 }
 
-// Check runs guppy's "check" mode which tests for errors in the configuration
-// arguments.
-func Check(args *lib.Args) {
-	ok := lib.Check(lib.GuppyMode, args)
-	if ok {
-		fmt.Println("No errors detected.")
-	}
-}
-
-// Convert runs guppy's "convert" mode, which converts files into/out of the
-// .gup format.
-func Convert(args *lib.Args) {
-	lib.Check(lib.GuppyMode, args)
+func Read(flags []string) {
+	set := flag.NewFlagSet("read", flag.ContinueOnError)
+	filePtr := set.String("file", "",
+		"The name of the guppy file to read.")
+	varStringPtr := set.String("vars", "",
+		"The name of the variables that will be read. Should be a comma-separated list.")
+	err := set.Parse(flags)
+	file, varString := *filePtr, *varStringPtr
+	vars := SplitCommaList(varString)
 	
-	thread.Set(args.Threads)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
 
-	for snap := 0; snap < args.Snaps; snap++ {
-		// Split particles into guppy data cubes.
-		origHd, part := lib.CollectParticles(args, snap)
-		buf := lib.SplitBuffer(args, part)
-			
-		for i, file := range args.GupFileNames {
-			// Split off
-			part.Split(args, i, buf)
-			
-			// Compress each file and write it to disk.
-			hd := lib.CreateHeader(args, origHd, i)
-			gupData := lib.Compress(args, buf)
-			lib.Write(args, file, hd, origHd, gupData)
-		}
+	hd, err := ReadHeader(file, vars)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
+	
+	err = PipeDataToStdout(file, hd, vars)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 	}
 }
 
-// Confirm run's guppy's "confirm" mode, which checks that
-func Confirm(args *lib.Args) {
-	lib.Check(lib.GuppyMode, args)
+func SplitCommaList(vars string) []string {
+	tokens := strings.Split(vars, ",")
+	for i := range tokens {
+		tokens[i] = strings.Trim(tokens[i], " ")
+	}
+	
+	out := []string{ }
+	for i := range tokens {
+		if len(tokens[i]) > 0 { out = append(out, tokens[i]) }
+	}
+	
+	return out
+}
 
-	thread.Set(args.Threads)
+func ReadHeader(file string, vars []string) (
+	hd *read_guppy.Header, err error) {
+	if file == "" {
+		return nil, fmt.Errorf("Must set the 'file' flag to run guppy in " +
+			"read mode. Call 'guppy read --help' for flag descriptions.")
+	} else if len(vars) == 0 {
+		return nil, fmt.Errorf("Must set the 'vars' flag to run guppy in " +
+			"read mode. Call 'guppy read --help' for flag descriptions.")
+	}
+	
+	if info, err := os.Stat(file); err != nil {
+		return nil, fmt.Errorf("Guppy could not open %s: " +
+			"%s", file, err.Error())
+	} else if info.IsDir() {
+		return nil, fmt.Errorf("Guppy could not open %s: " +
+			"it's a directory.", file)
+	}
 
-	for snap := 0; snap < args.Snaps; snap++ {
-		// This is the one weak link in the "confirm" chain. If CollectParticles
-		// has a bug in it, it might not be caught through "confirm".
-		origHd, part := lib.CollectParticles(args, snap)
+	// Need to go through these hoops because ReadHeader panics on error. This
+	// is the right call if you're calling those functions from C, but is
+	// annoying if you want to handle it cleanly.
+	err = nil
+	defer func() {
+		if panicData := recover(); panicData != nil {
+			panicMsg, ok := panicData.(string)
+			if !ok {
+				panic(fmt.Sprintf("Internal error: read_guppy panicked " +
+					"with something other than a string: %v", panicData))
+			}
+			err = fmt.Errorf(panicMsg)
+		}
+	}()
 
-		// Loop over all the .gup files.
-		for _, file := range args.GupFileNames {
-			f := read.Open(file)
-			lib.Confirm(origHd, part, f)
+	hd = read_guppy.ReadHeader(file)
+
+	for i, v := range vars {
+		if !IsValidVar(v, hd) {
+			return nil, fmt.Errorf("The %s requested variable, '%s', is not " +
+				"in the guppy file %s. This file can only read the " +
+				"variables %s, as well as derived variables, like " +
+				"'{RockstarParticle}', if the prerequisite variables exist " +
+				"in the file.", OrderString(i+1), v,
+				file, ArrayToCommaList(hd.Names))
 		}
 	}
-	fmt.Println("No errors detected.")
+
+	return hd, nil
+}
+
+func IsValidVar(v string, hd *read_guppy.Header) bool {
+	hasV, hasX := false, false
+	for i := range hd.Names {
+		if hd.Names[i] == "x{0}" { hasX = true }
+		if hd.Names[i] == "v{0}" { hasV = true }
+	}
+
+	if hasV && hasX && v == "{RockstarParticle}" { return true }
+
+	for i := range hd.Names {
+		if v == hd.Names[i] {
+			return true
+		}
+		j := strings.Index(hd.Names[i], "{0}")
+		if j >= 0 && hd.Names[i][:j] == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+func OrderString(n int) string {
+	switch n % 10 {
+	case 1: return fmt.Sprintf("%dst", n)
+	case 2: return fmt.Sprintf("%dnd", n)
+	case 3: return fmt.Sprintf("%drd", n)
+	}
+	return fmt.Sprintf("%dth", n)
+}
+
+func ArrayToCommaList(x []string) string {
+	return strings.Join(x, ", ")
+}
+
+func PipeDataToStdout(
+	file string, hd *read_guppy.Header, vars []string,
+) (err error) {
+	// As state above: read_guppy.go panics instead of returning errors to
+	// make C-users' lives easier. We need to convert those back into
+	// errors with this defer + recover().
+	defer func() {
+		if panicData := recover(); panicData != nil {
+			panicMsg, ok := panicData.(string)
+			if !ok {
+				panic(fmt.Sprintf("Internal error: read_guppy panicked " +
+					"with something other than a string: %v", panicData))
+			}
+			err = fmt.Errorf(panicMsg)
+		}
+	}()
+
+
+	read_guppy.InitWorkers(1)
+	err = WriteHeader(hd, os.Stdout)
+	if err != nil {
+		return fmt.Errorf("Could not write header: %s", err.Error())
+	}
+
+	for i := range vars {
+		buf := AllocateBuffer(vars[i], hd)
+		read_guppy.ReadVar(file, vars[i], 0, buf)
+		err = lib.WriteAsBytes(os.Stdout, buf)
+		if err != nil {
+			return fmt.Errorf("Could not write %s: %s", vars[i], err.Error())
+		}
+	}
+
+	return nil
+}
+
+func AllocateBuffer(v string, hd *read_guppy.Header) interface{} {
+	typeString := ""	
+	for i := range hd.Names {
+		if v == hd.Names[i] {
+			typeString = hd.Types[i]
+			break
+		}
+		j := strings.Index(hd.Names[i], "{0}")
+		if j >= 0 && v == hd.Names[i][:j] {
+			typeString = "v" + hd.Types[i][1:]
+			break
+		}
+	}
+	if v == "{RockstarParticle}" {
+		typeString = "{RockstarParticle}"
+	}
+
+	switch typeString {
+	case "u32": return make([]uint32, hd.N)
+	case "u64": return make([]uint64, hd.N)
+	case "f32": return make([]float32, hd.N)
+	case "f64": return make([]float64, hd.N)
+	case "v32": return make([][3]float32, hd.N)
+	case "v64": return make([][3]float64, hd.N)
+	case "{RockstarParticle}": return make([]lib.RockstarParticle, hd.N)
+	}
+
+	panic(fmt.Sprintf("Internal error: the variable '%s' passed correctness " +
+		"checks, but wasn't assigned a type string.", v))
+}
+
+type OutputHeader struct {
+    Version, Format uint64
+    N, NTot int64
+    Span, Origin, TotalSpan [3]int64
+    Z, OmegaM, OmegaL, H100, L, Mass float64
+}
+
+func WriteHeader(hd *read_guppy.Header, f *os.File) error {
+    ohd := &lib.PipeHeader{
+        lib.Version, lib.RockstarFormatCode, hd.N, hd.NTot,
+		hd.Span, hd.Offset, hd.TotalSpan,
+        hd.Z, hd.OmegaM, hd.OmegaL, hd.H100, hd.L, hd.Mass,
+    }
+	return binary.Write(f, lib.SystemByteOrder(), ohd)
+}
+
+
+func Write(flags []string) {
+	panic("NYI")
+}
+
+func WriteServer(flags []string) {
+	panic("NYI")
+}
+
+func ModeError() {
+	fmt.Fprintf(os.Stderr,
+`guppy requires at least a valid argument telling it what mode to run.
+Valid modes are:
+            read - reads particles from a file and writes them to stdout.
+    write_server - manages writer processes and helps them transfer particles
+                   between themselves.
+           write - writes particles to disk that it's either read from files on
+                   disk, or that have been written to its stdin.
+Run "./guppy <mode_name> --help to print help information about what flags a
+particular mode takes.%s`, "\n")
+	os.Exit(1)
 }
--- lib/lib.go
+++ lib/lib.go
@@ -1,3 +1,161 @@
-/*package lib contains various functions needed by guppy and mpi_guppy.
+/*package lib contains various functions needed by guppy and mpi_guppy. The
+functions in this particular package mainly just utility functions that might
+be useful for other programs manually piping output from Guppy. Almost all
+of the heavy lifting is done by lib/'s subpackages.
 */
 package lib
+
+import (
+	"encoding/binary"
+	"io"
+	
+	"reflect"
+	"unsafe"
+)
+
+var (
+	// Version is the version of the software. This can potentially be used
+	// to differentiate between breaking changes to the input/output format.
+	Version uint64 = 0x1
+	RockstarFormatCode uint64 = 0xffffffff00000001
+)
+
+// RockstarParticle is a particle with the structure expected by the
+// Rockstar halo finder.
+type RockstarParticle struct {
+	X, V [3]float32
+	ID uint64 
+}
+
+type PipeHeader struct {
+    Version, Format uint64
+    N, NTot int64
+    Span, Origin, TotalSpan [3]int64
+    Z, OmegaM, OmegaL, H100, L, Mass float64
+}
+
+func WriteAsBytes(f io.Writer, buf interface{}) error {
+	sysOrder := SystemByteOrder()
+	switch x := buf.(type) {
+	case []uint32: return binary.Write(f, sysOrder, x)
+	case []uint64: return binary.Write(f, sysOrder, x)
+	case []float32: return binary.Write(f, sysOrder, x)
+	case []float64: return binary.Write(f, sysOrder, x)
+	case [][3]float32:
+		// Go uses the reflect package to write non-primitive data through
+		// the binary package. This is slow and makes tons of heap allocations.
+		// So you need to be sneaky and "cast" to a primitive array.
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= 3
+        hd.Cap *= 3
+
+        f32x := *(*[]float32)(unsafe.Pointer(&hd))
+        err := binary.Write(f, sysOrder, f32x)
+
+        hd.Len /= 3
+        hd.Cap /= 3
+
+		return err
+		
+	case [][3]float64:
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= 3
+        hd.Cap *= 3
+
+        f64x := *(*[]float64)(unsafe.Pointer(&hd))
+        err := binary.Write(f, sysOrder, f64x)
+
+        hd.Len /= 3
+        hd.Cap /= 3
+
+		return err
+		
+	case []RockstarParticle:
+		particleSize := int(unsafe.Sizeof(RockstarParticle{ }))
+		
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= particleSize
+        hd.Cap *= particleSize
+
+		// RockstarParticle fields have inhomogenous sizes, so we need to
+		// convert to bytes.
+        bx := *(*[]byte)(unsafe.Pointer(&hd))
+        _, err := f.Write(bx)
+
+        hd.Len /= particleSize
+        hd.Cap /= particleSize
+
+		return err
+	}
+	
+	panic("Internal error: unrecognized type of interal buffer.")
+}
+
+func ReadAsBytes(f io.Reader, buf interface{}) error {
+	sysOrder := SystemByteOrder()
+	switch x := buf.(type) {
+	case []uint32: return binary.Read(f, sysOrder, x)
+	case []uint64: return binary.Read(f, sysOrder, x)
+	case []float32: return binary.Read(f, sysOrder, x)
+	case []float64: return binary.Read(f, sysOrder, x)
+	case [][3]float32:
+		// Go uses the reflect package to write non-primitive data through
+		// the binary package. This is slow and makes tons of heap allocations.
+		// So you need to be sneaky and "cast" to a primitive array.
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= 3
+        hd.Cap *= 3
+
+        f32x := *(*[]float32)(unsafe.Pointer(&hd))
+        err := binary.Read(f, sysOrder, f32x)
+
+        hd.Len /= 3
+        hd.Cap /= 3
+
+		return err
+		
+	case [][3]float64:
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= 3
+        hd.Cap *= 3
+
+        f64x := *(*[]float64)(unsafe.Pointer(&hd))
+        err := binary.Read(f, sysOrder, f64x)
+
+        hd.Len /= 3
+        hd.Cap /= 3
+
+		return err
+		
+	case []RockstarParticle:
+		particleSize := int(unsafe.Sizeof(RockstarParticle{ }))
+		
+		hd := *(*reflect.SliceHeader)(unsafe.Pointer(&x))
+        hd.Len *= particleSize
+        hd.Cap *= particleSize
+
+		// RockstarParticle fields have inhomogenous sizes, so we need to
+		// convert to bytes.
+        bx := *(*[]byte)(unsafe.Pointer(&hd))
+        _, err := io.ReadFull(f, bx)
+
+        hd.Len /= particleSize
+        hd.Cap /= particleSize
+
+		return err
+	}
+	
+	panic("Internal error: unrecognized type of interal buffer.")
+}
+
+func SystemByteOrder() binary.ByteOrder {
+	// See https://stackoverflow.com/questions/51332658/any-better-way-to-check-endianness-in-go/51332762
+	b := [2]byte{ }
+	*(*uint16)(unsafe.Pointer(&b[0])) = uint16(0x0001)
+	if b[0] == 0 {
+		return binary.BigEndian
+	} else {
+		return binary.LittleEndian
+	}
+}
+
